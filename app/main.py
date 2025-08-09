5be52a2a809849322229ef7e131ca0f804a8cbfa
Vladyslav Maidaniuk <vla.maidaniuk@gmail.com>
2025-08-09 14:13:39 +0200

    Init FastAPI + MongoDB with Docker Compose

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 import os
+
 app = FastAPI()
 client = MongoClient(os.getenv("MONGO_URL","mongodb://mongo:27017/app"))
 db = client.get_database()
+
 @app.get("/healthz")
 def healthz():
-    db.command("ping"); return {"status":"ok"}
+    db.command("ping")
+    return {"status":"ok"}
